b01ba0f3378a2b5dd57bcf50af9836487bda9ac2
Mikhail Grushko <grushko_m@Mikhail-Grushko.local>
2017-10-27 11:20:08 -0400

    fixed memes

--- firewall.py
+++ firewall.py
@@ -2,42 +2,37 @@ from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
 from pox.lib.util import dpidToStr
-from pox.lib.addresses import EthAddr
+from pox.lib.addresses import EthAddr, IPAddr
+import pox.lib.packet as pkt
 from collections import namedtuple
 import os
 import csv
-import pox.lib.packet as pkt
-from pox.lib.addresses import EthAddr, IPAddr
 
 log = core.getLogger()
-policyFile = "%s/pox/pox/misc/firewall-policies.csv" % os.environ[ 'HOME' ]  
-
-#Add your global variables here
-
-
+policyFile = "/pox/pox/misc/firewall-policies.csv"
 
 class Firewall (EventMixin):
 
     def __init__ (self):
         self.listenTo(core.openflow)
-        log.debug("Enabling Firewall Module")
-    self.firewall = {}
+        log.info("Enabling Firewall Module")
+        self.firewall = {}
 
     def sendRule (self, src, dst, duration = 0):
         if not isinstance(duration, tuple):
             duration = (duration,duration)
         msg = of.ofp_flow_mod()
-        match = of.ofp_match(dl_type = 0x800, nw_proto = pkt.ipv4.ICMP_PROTOCOL)
-    match.nw_src = EthAddr(src)
-    match.nw_dst = EthAddr(dst)
-    msg.match = match
-    msg.idle_timeout = duration[0]
-    msg.hard_timeout = duration[1]
-    msg.priority = 10
-    self.connection.send(msg)
+    match = of.ofp_match(dl_type = 0x800,
+                 nw_proto = pkt.ipv4.ICMP_PROTOCOL)
+        match.nw_src = IPAddr(src)
+        match.nw_dst = IPAddr(dst)
+        msg.match = match
+        msg.idle_timeout = duration[0]
+        msg.hard_timeout = duration[1]
+        msg.priority = 10
+        self.connection.send(msg)
 
-    # function that allows adding firewall rules into the firewall table
-    def AddRule (self, src, dst, value=True):
+    def AddRule (self, src=0, dst=0, value=True):
         if (src, dst) in self.firewall:
             log.info("Rule already present drop: src %s - dst %s", src, dst)
         else:
@@ -45,8 +40,7 @@ class Firewall (EventMixin):
             self.firewall[(src, dst)]=value
             self.sendRule(src, dst, 10000)
 
-    # function that allows deleting firewall rules from the firewall table
-    def DeleteRule (self, src, dst):
+    def DeleteRule (self, src=0, dst=0):
         try:
             del self.firewall[(src, dst)]
             sendRule(src, dst, 0)
@@ -54,33 +48,24 @@ class Firewall (EventMixin):
         except KeyError:
             log.error("Cannot find in rule drop src %s - dst %s", src, dst)
 
-    def _handle_ConnectionUp (self, event):    
-        #Add your logic here
-        #Add your logic here
+    def _handle_ConnectionUp (self, event):
         self.connection = event.connection
 
         ifile  = open(policyFile, "rb")
         reader = csv.reader(ifile)
         rownum = 0
         for row in reader:
-            # Save header row.
             if rownum == 0:
                 header = row
             else:
                 colnum = 0
                 for col in row:
-                    #print '%-8s: %s' % (header[colnum], col)
                     colnum += 1
                 self.AddRule(row[1], row[2])
             rownum += 1
         ifile.close()
 
         log.info("Firewall rules installed on %s", dpidToStr(event.dpid))
-        
-
-    
-        log.debug("Firewall rules installed on %s", dpidToStr(event.dpid))
 
 def launch ():
     core.registerNew(Firewall)
-
